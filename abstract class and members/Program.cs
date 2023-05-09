b48b613b0dbc72140124c37c1e379b98e3752d32
Md Asif Ikbal <75169639+asifikbal-inna@users.noreply.github.com>
2023-05-09 10:01:02 +0600

    Add files via upload

---
+++ abstract class and members/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+public abstract class Shape
+{
+    public abstract double CalculateArea(); // Abstract method
+
+    public void DisplayArea()
+    {
+        double area = CalculateArea();
+        Console.WriteLine("Area: " + area);
+    }
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double CalculateArea()
+    {
+        return length * width;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Shape circle = new Circle(3.5);
+        circle.DisplayArea(); // Output: Area: 38.48451000647496
+
+        Shape rectangle = new Rectangle(4.2, 6.8);
+        rectangle.DisplayArea(); // Output: Area: 28.559999999999995
+    }
+}
---
+++ access modifiers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class MyBaseClass
+{
+    private int privateField;
+
+    protected int protectedField;
+
+    public int publicField;
+
+    internal int internalField;
+
+    protected internal int protectedInternalField;
+
+    private void PrivateMethod()
+    {
+        Console.WriteLine("This is a private method.");
+    }
+
+    protected void ProtectedMethod()
+    {
+        Console.WriteLine("This is a protected method.");
+    }
+
+    public void PublicMethod()
+    {
+        Console.WriteLine("This is a public method.");
+    }
+
+    internal void InternalMethod()
+    {
+        Console.WriteLine("This is an internal method.");
+    }
+
+    protected internal void ProtectedInternalMethod()
+    {
+        Console.WriteLine("This is a protected internal method.");
+    }
+}
+
+public class MyDerivedClass : MyBaseClass
+{
+    public void AccessBaseClassMembers()
+    {
+        // Accessing inherited members from the base class
+        protectedField = 10;              // Accessing protected field
+        ProtectedMethod();                // Accessing protected method
+        publicField = 20;                 // Accessing public field
+        PublicMethod();                   // Accessing public method
+        internalField = 30;               // Accessing internal field
+        InternalMethod();                 // Accessing internal method
+        protectedInternalField = 40;      // Accessing protected internal field
+        ProtectedInternalMethod();        // Accessing protected internal method
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        MyDerivedClass myObject = new MyDerivedClass();
+
+        // Accessing members from the derived class
+        myObject.AccessBaseClassMembers();
+    }
+}
